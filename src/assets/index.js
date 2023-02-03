751ea126aa642d4820f361942f1fee3b057780da
Michael Aitken <mike@mikeaitken.net>
2023-02-03 19:39:21 +0800

    created top sections and fixed API

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,12 @@
 import loader from './loader.svg';
 import logo from './logo.svg';
 import noCoverArt from './nocoverart.jpg'
+import noArtistArt from './noartist.jpg'
 
 
 export {
   logo,
   loader,
   noCoverArt,
+  noArtistArt,
 };
